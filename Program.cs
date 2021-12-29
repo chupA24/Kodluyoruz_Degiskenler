ce15fed6197b0175d94c8d4961911eb644834e45
chupA24 <fatih_agdagli@hotmail.com>
2021-12-29 06:44:34 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool b1 = true;
+            bool b2 = false;
+
+            byte byte1 = new byte();
+            Console.WriteLine(byte1.ToString());
+
+            byte byte2 = 255;
+            Console.WriteLine(byte2.ToString());
+
+            sbyte sb1 = 8;
+
+            Int16 i16 =255;
+            Int32 i32 =23;
+
+            float fl = 2.5f;
+            double d1 = 9d;
+            Console.WriteLine(fl.ToString());
+            Console.WriteLine(d1.ToString()); 
+
+        }
+    }
+}
